65d2abf115b25bef93847b006a99497898381832
Edgard Levy <edgardlevy@gmail.com>
2020-05-09 23:48:18 -0300

    - extendendo funcoes no repositorio de agendamentos

--- primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
+++ primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
@@ -1,3 +1,30 @@
+import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointments';
 
-class AppointmentsRepository {}
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+    this.appointments.push(appointment);
+    return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment =
+      this.appointments.find(appointment => isEqual(date, appointment.date)) ||
+      null;
+
+    return findAppointment;
+  }
+
+  public index(): Appointment[] {
+    return this.appointments;
+  }
+}
+
+export default AppointmentsRepository;
--- primeiro-projeto-node/src/routes/appointments.router.ts
+++ primeiro-projeto-node/src/routes/appointments.router.ts
@@ -1,22 +1,24 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointments';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 // const appointments: Array<Appointment> = [];
-const appointments: Appointment[] = [];
+// const appointments: Appointment[] = [];
 
 appointmentsRouter.get('/', (request, response) => {
-  return response.json(appointments);
+  return response.json(appointmentsRepository.index());
 });
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentInSameDate) {
@@ -25,8 +27,8 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
+
   return response.json(appointment);
 });
 
